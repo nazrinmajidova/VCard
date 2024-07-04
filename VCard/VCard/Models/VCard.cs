d00287bc5e703d95815c3a25e6d30ab5fe51fc4a
nazrinmajidova <150728280+nazrinmajidova@users.noreply.github.com>
2024-07-04 20:42:55 +0400

    VCard mini proje

---
+++ VCard/VCard/Models/VCard.cs
@@ -0,0 +1,65 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static VCardProject.Program;
+
+namespace VCardProject.Models
+{
+    public class VCard
+    {
+        public Name Name { get; set; }
+        public Location Location { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+
+
+        // VCard-ı string kimi formatlaşdıran method
+        public string VCardFormat()
+        {
+            return $"BEGIN         :VCARD\r\n" +
+                   $"VERSION       :3.0\r\n" +
+                   $"FN            :{Name.First} {Name.Last}\r\n" +
+                   $"EMAIL         :{Email}\r\n" +
+                   $"TEL           :{Phone}\r\n" +
+                   $"ADR;TYPE=HOME :;;{City};;{Country}\r\n" +
+                   $"END:VCARD";
+
+        }
+
+        // VCard stringi file`a vcf file şəklində save etmək üçün static method
+        public static void SaveMethod(string VCardString, string fileName, string directory)
+        {
+            var Directory = $@"{directory}";
+            var Filename = $"{fileName}.vcf";
+            var fullPath = Path.Combine(Directory, Filename);
+            File.WriteAllText(fullPath, VCardString);
+        }
+    }
+
+    // APİ- dan alınmış JSON responseları göstərən class
+    public class RandomUserResponse
+    {
+        [JsonProperty("results")]
+        public VCard[] Results { get; set; }
+    }
+
+
+    public class Name
+    {
+        public string First { get; set; }
+        public string Last { get; set; }
+    }
+
+    public class Location
+    {
+        public string Country { get; set; }
+        public string City { get; set; }
+    }
+
+}
+
---
+++ VCard/VCard/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using VCardProject.Models;
+using static VCardProject.Program;
+
+namespace VCardProject
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // VCard filelarının save olunduğu directory
+            var FilePath = "D:\\Back-End\\VCard\\VCard\\VCards";
+            try
+            {
+
+                Console.Write("Please enter the number of vCards you want to create: ");
+                int size = Convert.ToInt32(Console.ReadLine());
+
+                // random user datanı aldığımız url
+                string url = $"https://randomuser.me/api/?results={size}";
+
+                // APİ requestləri yaratmaq üçün HttpClient yaradırıq 
+                using HttpClient client = new HttpClient();
+                using HttpResponseMessage response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+
+                string responseBody = await response.Content.ReadAsStringAsync();
+
+                // JSON responseəları RandomUserResponse obyektinə deserialize edirik
+                var users = JsonConvert.DeserializeObject<RandomUserResponse>(responseBody);
+
+
+                foreach (var vCard in users.Results)
+                {
+                    string vCardString = vCard.VCardFormat();
+                    Console.WriteLine(vCardString); // Vcardı string şəkildə console da göstərir
+
+                    // VCard stringi file a save edir 
+                    VCard.SaveMethod(vCardString, $"{vCard.Name.First}{vCard.Name.Last}", FilePath);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+
+        }
+
+
+    }
+
+
+}
